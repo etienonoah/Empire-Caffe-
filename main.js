2353445f8f09550b0ed926a3a2ec4d30634371f2
etienonoah <etienonoah@gmail.com>
2026-02-19 20:57:04 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('butt').addEventListener('click',function()
+{window.location.href ='menu.html'})
+
+document.getElementById('buy-now').onclick = function(){
+    window.location.href='checkout.html';
+};
