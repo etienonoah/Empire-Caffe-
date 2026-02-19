8618ae91834312eb99930fab220bd06f2a44b149
etienonoah <etienonoah@gmail.com>
2026-02-19 23:53:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 document.getElementById('butt').addEventListener('click',function()
 {window.location.href ='menu.html'})
 
-document.getElementById('buy-now').onclick = function(){
-    window.location.href='checkout.html';
-};
