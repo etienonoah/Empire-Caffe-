0df3fb6157eb8a1bd89254e30b55f00be811f37b
etienonoah <etienonoah@gmail.com>
2026-02-20 05:03:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
 document.getElementById('butt').addEventListener('click',function()
 {window.location.href ='menu.html'})
 
-document.querySelectorAll(".button").forEach(button => {
-
-  button.addEventListener("click", function(){
-
-    const product = this.dataset.product;
-    const price = this.dataset.price;
-
-    localStorage.setItem("product", product);
-    localStorage.setItem("price", price);
-
-    window.location.href = "checkout.html";
-
-  });
-
